361d0df3cd0f02cf02a124ef742a2acb8fdecd75
ogigoc <ognjen@Anas-MacBook-Pro.local>
2017-07-28 23:00:10 +0200

    Presentation start data.

--- plot.py
+++ plot.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
-basic_rnn_loss = [float(l) for l in open('ap_basic_rnn_loss.txt', 'r').read().split('\n')]
-lstm_loss = [float(l) for l in open('ap_ap_lstm_loss.txt', 'r').read().split('\n')]
-lstm100_loss = [float(l) for l in open('lstm100_ap_loss.txt', 'r').read().split('\n')]
+def deserialize(path):
+	loss = [float(l) for l in open(path, 'r').read().split('\n')[:-1]]
+
+ap_lstm100 = deserialize('presentation/')
 
 plt.plot(lstm100_loss, 'r-')
 plt.plot(lstm_loss, 'b-')
